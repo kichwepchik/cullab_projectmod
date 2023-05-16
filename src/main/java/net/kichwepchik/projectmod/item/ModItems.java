3dcb307bbe3c2a56b3fc1cea70e2b1ff3f7aae91
kichwepchik <roma2005best@gmail.con>
2023-05-16 18:22:46 +0200

    beta 0.0.2

--- src/main/java/net/kichwepchik/projectmod/item/ModItems.java
+++ src/main/java/net/kichwepchik/projectmod/item/ModItems.java
@@ -1,10 +1,11 @@
 package net.kichwepchik.projectmod.item;
 
 import net.kichwepchik.projectmod.ProjectMod;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
@@ -22,6 +23,48 @@ public class ModItems {
 
     public static final RegistryObject<Item> TITAN_INGOT = ITEMS.register("titan_ingot",
             ()->new Item(new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+
+
+    public static final RegistryObject<Item> GHOST_HEART = ITEMS.register("ghost_heart",
+            ()->new Item(new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+
+
+    public static final RegistryObject<Item> PIECE_OF_SOUL = ITEMS.register("piece_of_soul",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+
+
+
+
+    public static final  RegistryObject<Item> TITAN_SWORD = ITEMS.register("titan_sword",
+            ()-> new SwordItem(ModTiers.TITAN,2,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+    public static final  RegistryObject<Item> TITAN_PICKAXE = ITEMS.register("titan_pickaxe",
+            ()-> new PickaxeItem(ModTiers.TITAN,2,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+    public static final  RegistryObject<Item> TITAN_SHOVEL = ITEMS.register("titan_shovel",
+            ()-> new ShovelItem(ModTiers.TITAN,2,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+    public static final  RegistryObject<Item> TITAN_AXE = ITEMS.register("titan_axe",
+            ()-> new AxeItem(ModTiers.TITAN,2,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+    public static final  RegistryObject<Item> TITAN_HOE = ITEMS.register("titan_hoe",
+            ()-> new HoeItem(ModTiers.TITAN,2,3f,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+
+
+    public static final  RegistryObject<Item> TITAN_HELMET = ITEMS.register("titan_helmet",
+            ()-> new ArmorItem(ModArmorMaterials.TITAN, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+ public static final  RegistryObject<Item> TITAN_CHESTPLATE = ITEMS.register("titan_chestplate",
+            ()-> new ArmorItem(ModArmorMaterials.TITAN, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+ public static final  RegistryObject<Item> TITAN_LEGGING = ITEMS.register("titan_leggings",
+            ()-> new ArmorItem(ModArmorMaterials.TITAN, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+ public static final  RegistryObject<Item> TITAN_BOOTS = ITEMS.register("titan_boots",
+            ()-> new ArmorItem(ModArmorMaterials.TITAN, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.PROJECT_TAB)));
+
 
 
 
---
+++ src/main/java/net/kichwepchik/projectmod/item/ModTiers.java
@@ -0,0 +1,12 @@
+package net.kichwepchik.projectmod.item;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+
+public class ModTiers {
+    public static final ForgeTier TITAN = new ForgeTier(4,2500,11f,
+            2f,22, BlockTags.NEEDS_DIAMOND_TOOL,
+            ()-> Ingredient.of(ModItems.TITAN_INGOT.get()));
+    
+}
